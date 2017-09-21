d1631170676745e94428d3f9c8e88decb1b2168f
jimburk <Jim@JBurk.net>
2017-09-21 15:45:58 -0700

    Almost done, no second screen

--- app/src/main/java/edu/orangecoastcollege/cs273/jburk/ocmusicevents/EventsListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/jburk/ocmusicevents/EventsListActivity.java
@@ -1,13 +1,30 @@
 package edu.orangecoastcollege.cs273.jburk.ocmusicevents;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventsListActivity extends AppCompatActivity {
+public class EventsListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events_list);
+        //setContentView(R.layout.activity_events_list);
+
+        //Define a built-in list adaptor
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles));
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String title = MusicEvent.titles[position];
+        String details = MusicEvent.details[position];
+
+        Intent detailsIntent = new Intent(this, EventsListActivity.class);
+        detailsIntent.putExtra("title", title);
+        detailsIntent.putExtra("details", details);
     }
 }
